8b177b5882fc59ee8cec0a4c813fbda2ae9788fb
Fernando <fernando.moreno@fcen.uba.ar>
2025-12-03 07:43:29 -0300

    Add debug ECS

--- terraform/ecs-deploy/ecr.tf
+++ terraform/ecs-deploy/ecr.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "frontend" {
   name = "${var.project_name}-frontend"
-
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -8,6 +8,7 @@ resource "aws_ecr_repository" "frontend" {
 
 resource "aws_ecr_repository" "grafana" {
   name = "${var.project_name}-grafana"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -16,6 +17,7 @@ resource "aws_ecr_repository" "grafana" {
 
 resource "aws_ecr_repository" "prometheus" {
   name = "${var.project_name}-prometheus"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
